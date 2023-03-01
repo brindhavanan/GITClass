6b45a0f804e6cbfbdf818596fc4969a972d9085e
brindhavanan <brindhavanan@gmail.com>
2023-03-01 12:33:51 +0530

    john updated code

--- src/test/java/com/git/Class1.java
+++ src/test/java/com/git/Class1.java
@@ -12,5 +12,17 @@ public class Class1 {
 	private void method3() {
 		System.out.println("3");
 	}
+	
+	private void method7() {
+		System.out.println("7");
+	}
+
+	private void method8() {
+		System.out.println("8");
+	}
+
+	private void method9() {
+		System.out.println("9");
+	}
 
 }
